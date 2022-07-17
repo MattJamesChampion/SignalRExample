b65be7c09c74d2050aba1d790f22df15a471dfae
Matthew Champion <mattjameschampion@gmail.com>
2022-07-17 10:11:34 +0100

    Add SignalR example

---
+++ SignalRExample/SignalR/Hubs.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRExample.SignalR;
+
+public class ChatHubInstance : Hub
+{
+    public async Task SendChatMessage(string user, string chatMessage)
+    {
+        await Clients.All.SendAsync("ReceiveChatMessage", user, chatMessage);
+    }
+}
---
+++ SignalRExample/wwwroot/js/chat.js
@@ -0,0 +1,32 @@
+﻿"use strict";
+
+// Sourced from https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr#add-signalr-client-code
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/ChatHubEndpoint").build();
+
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveChatMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("chatMessagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var chatMessage = document.getElementById("chatMessageInput").value;
+    connection.invoke("SendChatMessage", user, chatMessage).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
